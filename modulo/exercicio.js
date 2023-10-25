439c1010011f325f8ccfce055d7675bf39a97244
wally 🧨🎇 <ricardoborges21122006@gmail.com>
2023-10-24 23:47:39 -0300

    1,2,3,4,5 finalizado

--- modulo/exercicio.js
+++ modulo/exercicio.js
@@ -1,23 +1,11 @@
-var estados_cidades = require ('./estados_cidades.js')
-
-// let cont = 0;
-    //while (cont < listaDeProdutos.length){
-        //console.log (`nome do Produto: ${listaDeProdutos[cont]}`);
-       // cont +=1;
-   //}
-
-   //FOR
-   //for(let cont=0 ; cont <listaDeProdutos.length; cont++){
-    //console.log (`nome do Produto: ${listaDeProdutos[cont]}`);
-
-   //}
+var estadosCidades = require ('./estados_cidades.js')
 
     const getListadeEstados= function(){
 
         let arraySigla = []
         let JSONSigla = {}
-        for (let cont=0; cont < estados_cidades.estadosCidades.estados.length; cont++ ){
-            arraySigla.push(estados_cidades.estadosCidades.estados[cont].sigla)
+        for (let cont=0; cont < estadosCidades.estadosCidades.estados.length; cont++ ){
+            arraySigla.push(estadosCidades.estadosCidades.estados[cont].sigla)
             
         }
 
@@ -29,19 +17,11 @@ var estados_cidades = require ('./estados_cidades.js')
         
     };
 
-    //FOREACH -> estrutura de repetição feita para trabalhar com array, ele gerencia o índice, ele pega o arrray "Lista de produtos" 
-    //e ele cria na memoria uma replica dsse array, pegando o conteudo do array e joga numa variavel de callback
-
-   //listaDeProdutos.forEach(function(produto){
-    //console.log (`Nome do Produto: ${produto}`)
-
-    //})
-
     const getDadosEstado = function() {
         let JSONDados = {};
         let filtro = 'RJ';
 
-        estados_cidades.estadosCidades.estados.forEach(function(estado) {
+        estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro === estado.sigla) {
                 JSONDados.uf = estado.sigla;
                 JSONDados.descricao = estado.nome;
@@ -53,13 +33,12 @@ var estados_cidades = require ('./estados_cidades.js')
     
         return JSONDados;
     };
-    
 
     const getCapitalEstado = function() {
         let JSONCapital = {};
         let filtro = 'SP';
     
-        estados_cidades.estadosCidades.estados.forEach(function(estado) {
+        estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro.toUpperCase() === estado.sigla.toUpperCase()) {
                 JSONCapital.uf = estado.sigla.toUpperCase();
                 JSONCapital.descricao = estado.nome;
@@ -74,7 +53,7 @@ var estados_cidades = require ('./estados_cidades.js')
         let arrayEstados = [];
         let filtro = 'SUL';
     
-        estados_cidades.estadosCidades.estados.forEach(function(estado) {
+        estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro.toUpperCase() === estado.regiao.toUpperCase()) {
                 let JSONEstados = {};
                 JSONEstados.uf = estado.sigla;
@@ -89,13 +68,13 @@ var estados_cidades = require ('./estados_cidades.js')
         return JSONRegiao;
     };
 
-
-const getCapitalPais = function() {
-    let JSONCapitais = {};
-    let arrayCapitais = [];
-
-    if (estados_cidades && estados_cidades.estados && Array.isArray(estados_cidades.estados) && estados_cidades.estados.length > 0) {
-        estados_cidades.estadosCidades.estados.forEach(function(estado) {
+    const getCapitalPais = function() {
+        let JSONCapitais = {};
+        let arrayCapitais = [];
+    
+        for (let contador = 0; contador < estadosCidades.estadosCidades.estados.length; contador++) {
+            const estado = estadosCidades.estadosCidades.estados[contador];
+    
             if (estado.capital_pais) {
                 let JSONEstados = {
                     capital_atual: estado.capital_pais.capital,
@@ -108,44 +87,36 @@ const getCapitalPais = function() {
                 };
                 arrayCapitais.push(JSONEstados);
             }
-        });
-    } 
-
-    JSONCapitais.capitais = arrayCapitais;
-    return JSONCapitais;
-};
-
-const getCidades = function() {
-    let JSONCidades = {};
-    let arrayCidades = [];
-    let filtro = 'AC';
-
-    // Verificar se estados_cidades.estados é um array e contém dados
-    if (estados_cidades && estados_cidades.estados && Array.isArray(estados_cidades.estados)) {
-        estados_cidades.estados.forEach(function(estado) {
-            if (filtro === estado.sigla) {
-                JSONCidades.uf = estado.sigla;
-                JSONCidades.descricao = estado.nome;
-                JSONCidades.quantidade_cidades = estado.cidades.length;
-
-                // Adicionar nomes das cidades ao arrayCidades
+        }
+    
+        JSONCapitais.capitais = arrayCapitais;
+        return JSONCapitais;
+    };
+     
+    const getCidades = function() {
+        let JSONCidades = {};
+        let arrayCidades = [];
+        let filtro = 'AC';
+        let encontrado = false;
+    
+        estadosCidades.estadosCidades.estados.forEach(function(estado) {
+            if (estado.sigla === filtro && !encontrado) {
                 estado.cidades.forEach(function(cidade) {
                     arrayCidades.push(cidade.nome);
                 });
-
+    
+                JSONCidades.uf = estado.sigla;
+                JSONCidades.descricao = estado.nome;
+                JSONCidades.quantidade_cidades = estado.cidades.length;
                 JSONCidades.cidades = arrayCidades;
+                encontrado = true; 
             }
         });
-    } else {
-        console.log("Os dados de estados não estão definidos ou são inválidos.");
-        return null;
-    }
-
-    return JSONCidades;
-};
-
-
-
+    
+        return JSONCidades;
+    };
+    
+    
     //console.log(getCidades());
     //console.log(getCapitalPais());
     //console.log(getEstadosRegiao());
