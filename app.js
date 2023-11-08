a163bf524ddd8bd2236f418d47f86d037f653802
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2023-11-08 12:26:57 -0300

    5 apis funcionando

--- app.js
+++ app.js
@@ -52,17 +52,95 @@ app.use((request,response,next) =>{
     next();
 })
 
-//EndPoints: 
 
+//EndPoints: Lista a sigla de todos os estados
 app.get('/estados/sigla', cors (), async function (request,response,next){
     
     let controleEstadosCidades = require('./modulo/exercicio.js');
     let listaEstados = controleEstadosCidades.getListadeEstados()
 
-    response.json(listaEstados);
+
+    if (listaEstados){ 
+        response.json(listaEstados);
     response.status(200);
+    }else{
+        response.status(404);
+    }
 })
+
+//EndPoints: Retorna dados do estado filtrando pela sigla
+app.get('/estado/sigla/:uf', cors(), async function(request,response,next){
+
+    // recebe uma váriavel encaminhada como parametro na requisição
+    let siglaEstado = request.params.uf;
+
+    let controleDadosEstados = require ('./modulo/exercicio.js');
+    let dadosEstado = controleDadosEstados.getDadosEstado(siglaEstado);
+    
+    if(dadosEstado){
+        response.json(dadosEstado);
+        response.status(200);
+    }else{
+        response.status(404);
+        response.json({erro: 'item não encontrado' })
+    }
+
+})
+
+//EndPoints: Retorna dados da capital filtrando pela sigla do estado
+app.get('/capital/estado', cors(), async function(request,response,next){
+
+    // recebe uma váriavel encaminhada como QueryString na requisição
+    // EX: '/capital/estado?UF=SP
+    let siglaEstado = request.query.uf;
+
+    let controleDadosCapital = require ('./modulo/exercicio.js');
+    let dadosEstado = controleDadosCapital.getCapitalEstado(siglaEstado);
+    
+    if(dadosEstado){
+        response.json(dadosEstado);
+        response.status(200);
+    }else{
+        response.status(404);
+        response.json({erro: 'item não encontrado' })
+    }
+
+})
+
+
+app.get('/estados/regiao', cors(), async function(request,response,next) {
+    let regiao = request.query.regiao;
+
+    let controleEstadosRegiao = require ('./modulo/exercicio.js');
+    let dadosRegiao = controleEstadosRegiao.getEstadosRegiao(regiao);
+
+    if(dadosRegiao){
+        response.json(dadosRegiao);
+        response.status(200);
+    } else {
+        reponse.status(404);
+        response.json({erro: 'item não encontrado' })
+    }
+})
+
+app.get('/capital/pais', cors(), async function(request,response,next){
+    let capital = request.query.capital;
+
+    let controleCapitalPais = require ('./modulo/exercicio.js');
+    let dadosCapital = controleCapitalPais.getCapitalPais(capital);
+
+    if(dadosCapital){
+        response.json(dadosCapital);
+        response.status(200);
+    }else {
+        reponse.status(404);
+        response.json({erro: 'item não encontrado' })
+    }
+} )
+
+
 
 app.listen('8080', function(){
     console.log('API FUNCIONANDO')
 })
+
--- modulo/exercicio.js
+++ modulo/exercicio.js
@@ -26,22 +26,26 @@ var estadosCidades = require ('./estados_cidades.js')
 
         let arraySigla = []
         let JSONSigla = {}
+        let status = false;
         for (let cont=0; cont < estadosCidades.estadosCidades.estados.length; cont++ ){
             arraySigla.push(estadosCidades.estadosCidades.estados[cont].sigla)
-            
+            status = true
         }
 
         JSONSigla.estados = arraySigla
         JSONSigla.count = arraySigla.length
         
-
-        return JSONSigla;
+        if (status)
+            return JSONSigla;
+        else 
+            return false;
         
     };
 
     const getDadosEstado = function() {
         let JSONDados = {};
-        let filtro = 'RJ';
+        let filtro = 'DF';
+        let status = false;
 
         estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro === estado.sigla) {
@@ -49,51 +53,70 @@ var estadosCidades = require ('./estados_cidades.js')
                 JSONDados.descricao = estado.nome;
                 JSONDados.capital = estado.capital;
                 JSONDados.regiao = estado.regiao;
-                
+                status = true
             }
         });
     
-        return JSONDados;
+        if (status)
+            return JSONDados;
+        else 
+            return false;
+    
     };
 
     const getCapitalEstado = function() {
         let JSONCapital = {};
         let filtro = 'SP';
+        let status = false;
     
         estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro.toUpperCase() === estado.sigla.toUpperCase()) {
                 JSONCapital.uf = estado.sigla.toUpperCase();
                 JSONCapital.descricao = estado.nome;
-                JSONCapital.capital = estado.capital; 
+                JSONCapital.capital = estado.capital;
+                status = true 
             }
         });
+
+        if (status)
         return JSONCapital;
+    else 
+        return false;
+
     };
 
     const getEstadosRegiao = function() {
         let JSONRegiao = {};
         let arrayEstados = [];
         let filtro = 'SUL';
-    
+        let status = false;
+
         estadosCidades.estadosCidades.estados.forEach(function(estado) {
             if (filtro.toUpperCase() === estado.regiao.toUpperCase()) {
                 let JSONEstados = {};
                 JSONEstados.uf = estado.sigla;
                 JSONEstados.nome = estado.nome;
                 arrayEstados.push(JSONEstados);
+                status = true 
             }
         });
         if (arrayEstados.length > 0) {
             JSONRegiao.regiao = filtro.toUpperCase();
             JSONRegiao.estados = arrayEstados;
         }
+
+        if (status)
         return JSONRegiao;
+    else 
+        return false;
+        
     };
 
     const getCapitalPais = function() {
         let JSONCapitais = {};
         let arrayCapitais = [];
-    
+        let status = false;
+
         for (let contador = 0; contador < estadosCidades.estadosCidades.estados.length; contador++) {
             const estado = estadosCidades.estadosCidades.estados[contador];
     
@@ -108,11 +131,16 @@ var estadosCidades = require ('./estados_cidades.js')
                     capital_pais_ano_termino: estado.capital_pais.ano_fim
                 };
                 arrayCapitais.push(JSONEstados);
+                status = true;
             }
         }
     
         JSONCapitais.capitais = arrayCapitais;
+        if (status)
         return JSONCapitais;
+    else 
+        return false;
+
     };
      
     const getCidades = function() {
@@ -139,14 +167,14 @@ var estadosCidades = require ('./estados_cidades.js')
     };
     
 
-    console.log(getCidades());
-    console.log(getCapitalPais());
-    console.log(getEstadosRegiao());
-    console.log(getCapitalEstado());
-    console.log(getDadosEstado());
-    console.log(getListadeEstados());
+    //console.log(getCidades());
+    //console.log(getCapitalPais());
+    //console.log(getEstadosRegiao());
+    //console.log(getCapitalEstado());
+    //console.log(getDadosEstado());
+    //console.log(getListadeEstados());
 
-   module.exports = {getListadeEstados}
+   module.exports = {getListadeEstados,getCidades,getCapitalPais,getEstadosRegiao,getCapitalEstado,getDadosEstado}
 
 
 
